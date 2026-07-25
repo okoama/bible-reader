27f2ac4df01c951ab8412019dbc942e40dbdf830
Amarachi Okoli <amaranitaok@gmail.com>
2026-07-25 15:42:43 -0500

    Created barrel exports

---
+++ bible-reader/types/index.ts
@@ -0,0 +1,6 @@
+export type { Note } from './note';
+export type { Highlight } from './highlight';
+export type { Bookmark } from './bookmark';
+export type { Prayer } from './prayer';
+export type { ReadingProgress } from './readingProgress';
+export type { SourceReference } from './sourceReference';
